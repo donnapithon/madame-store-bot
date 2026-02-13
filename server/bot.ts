7e5ea5602f82a447341d48e57c7b7a12c3b3d35e
donnapithon <pixellahalls@gmail.com>
2026-02-13 18:50:36 -0300

    Update bot.ts

--- server/bot.ts
+++ server/bot.ts
@@ -1018,8 +1018,8 @@ export async function startBot(webhookDomain?: string) {
 }
 
 bot.on('document', async (ctx) => {
-  console.log("DOCUMENT RECEBIDO DE:", ctx.from?.id);
-console.log("ADMIN_ID:", ADMIN_ID);
+console.log("USER ID:", ctx.from?.id);
+console.log("ADMIN ID:", ADMIN_ID);
 
   const state = userStates.get(ctx.from.id);
   if (!state || (state.step !== 'add_bulk_stock' && state.step !== 'add_stock_content')) return;
